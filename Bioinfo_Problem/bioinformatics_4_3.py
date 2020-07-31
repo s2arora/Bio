3042cd97a29e056579b1d56d720ba8c39521b072
s2arora <s2arora@s2arora.local>
2020-08-01 00:54:19 +0900

    modified

--- Bioinfo_Problem/bioinformatics_4_3.py
+++ Bioinfo_Problem/bioinformatics_4_3.py
@@ -1,31 +1,26 @@
-cnt = 0
 ORIGIN_passed = False
 Origin_trace = []
 
 with open("sequence.protein.gb",'r') as fr:
 	for line in fr:
-		cnt += 1
-		if cnt == 1:
-			title = line 
-		
 		if line.startswith("ORIGIN"):
 			ORIGIN_passed = True
 		elif ORIGIN_passed:
 			Origin_trace.append(line[10:-1])
-			
-protein = ""
+
+#Origin_trace is protein line list [have a space]
+
+protein_word = ""
 for protein_line in Origin_trace:
-	protein += protein_line.replace(" ",'') 
+	protein_word += protein_line.replace(" ",'')
 
-cnt2 = 0
-count70 = ""
+count = 0
 
-for one_protein in protein:
-	cnt2 += 1
-	if cnt2 % 70 == 0:
-		count70 += cnt2
-	
-print(count70)
+for i in protein_word:
+	print (i, end= '')
+	count += 1
+	if count == 70:
+		print("")
+		count = 0
 
-	
-
+print("")
