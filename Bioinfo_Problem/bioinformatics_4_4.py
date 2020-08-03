5b8cf75c34c33382a19ec8fd9ab0733ea85ca116
s2arora <s2arora@s2arora.local>
2020-08-03 11:18:52 +0900

    modified2

--- Bioinfo_Problem/bioinformatics_4_4.py
+++ Bioinfo_Problem/bioinformatics_4_4.py
@@ -1,3 +1,16 @@
+def line_70_cut(string, cnt=70) :
+    count = 0
+    temp = ""
+    seq_list = []
+    for c in string:
+        count += 1
+        temp += c
+        if count % cnt == 0:
+            seq_list.append(temp)
+            temp = ""
+    seq_list.append(temp)
+    return seq_list
+
 input_fileName = input("Enter input file: ")
 make_fileName = input("Enter output file: ")
 
@@ -6,10 +19,9 @@ print("Option-2) Read a FASTA format DNA sequence file and make a reverse comple
 print("Option-3) Convert GenBank format file to FASTA format file.")
 
 select = int(input("Select the option: "))
-
-seq = ""
 if input_fileName == "sequence.nucleotide.fasta":    
     with open(input_fileName,"r") as fr:
+        seq = ""
         for line in fr:
             if line.startswith(">"):
                 title = line
@@ -19,7 +31,8 @@ if input_fileName == "sequence.nucleotide.fasta":
     sequence_reversed = ''.join(reversed(seq))
     
     if select == 1:
-        sequence1 = title + sequence_reversed + "\n"
+        seq_list = line_70_cut(sequence_reversed)
+        sequence1 = title + ('\n'.join(seq_list)) + "\n"
         with open(make_fileName, 'w') as fw:
             fw.write(sequence1)
 
@@ -32,8 +45,8 @@ if input_fileName == "sequence.nucleotide.fasta":
     if select == 2:
         for seq_rev in sequence_reversed:
             sequence_reversed_complement += sequence_ATGC[seq_rev]
-
-        sequence2 = title + sequence_reversed_complement + "\n"
+        seq_list = line_70_cut(sequence_reversed_complement)
+        sequence2 = title + ('\n'.join(seq_list)) + "\n"
         with open(make_fileName, 'w') as fw:
             fw.write(sequence2)
 
@@ -53,19 +66,11 @@ elif input_fileName == "sequence.nucleotide.gb":
                 seq.append(line[10:-1])
 
     if select == 3:
-        print("seq", seq)
-        
         sequence = ""
         for sequence_line in seq:
-            sequence += sequence_line.replace("", '')
-        print("sequence", sequence)
-        sequence3 = title + sequence + "\n"
+            sequence += sequence_line.replace(" ", '')
+        seq_list = line_70_cut(sequence)
+        sequence3 = title + ('\n'.join(seq_list)) + "\n"
 
         with open(make_fileName,'w') as fw:
             fw.write(sequence3)            
-
-
-'''
-if select == 3:
-    with open(make_fileName, 'w') as fw:
-'''      
